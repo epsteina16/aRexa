683377ec50cfee459bac58781b98c712e09aeb07
Josh Simani <jsimani@gmail.com>
2018-10-13 02:29:08 -0400

    Fixed outputSpeech data

--- api/startup.js
+++ api/startup.js
@@ -14,7 +14,9 @@ app.use(bodyParser.json());
 //app.use("/songs", song_controller);
 
 app.post("/initialize", function(req, res) {
-    res.send("Welcome to aRexa! Would you like to start a new session or resume from an existing session?");
+    var info = new Object();
+    info.outputSpeech = "Welcome to aRexa! Would you like to start a new session or resume from an existing session?";
+    res.send(JSON.stringify(info));
 });
 
 app.post("/start", function(req, res) {
@@ -39,26 +41,34 @@ app.post("/start_new", function(req, res) {
 
     var new_sessionID = body.session.sessionID;
     var new_session = new session({sessionID: new_sessionID});
-    
-    if (body.intent.name == "new_song") {
-        var artist = body.intent.slots.Artist.value;
-        $.get("https://api.spotify.com/v1/search?q=" + artist + "&type=artist&limit=1", function(data){
-            var song = body.intent.slots.Song.value;
-            $
-            $.get("https://api.spotify.com/v1/recommendations?seed_artists")
-        })
-        
+    var userID = "nikhilbl";
+    $.get("https://api.spotify.com/v1/users/" + userID + "/playlists?name=arexa_playlist",function(playlist_id){
+        if (body.intent.name == "new_song") {
+            var artist = body.intent.slots.Artist.value;
+            $.get("https://api.spotify.com/v1/search?q=" + artist + "&type=artist&limit=1", function(artist_data){
+                var song = body.intent.slots.Song.value;
+                $.get("https://api.spotify.com/v1/search?q=" + song + "&type=track&limit=1", function(song_data){
+                    $.get("https://api.spotify.com/v1/recommendations?seed_artists=" + artist_data.id + "&seed_tracks=" + song_data.id + "&limit=5", function(seed_data){
+                        $.post("https://api.spotify.com/v1/playlists/" + playlist_id.id + "/tracks?uris=spotify:track:" + song_data.id + ",, function(data){
 
-    } else if (body.intent.name == "new_artist") {
-        var artist = body.intent.slots.Artist.value;
+                        }
+                    }
+                })
+            })
+            
 
-    } else if (body.intent.name == "new_genre") {
-        var genre = body.intent.slots.Genre.value;
+        } else if (body.intent.name == "new_artist") {
+            var artist = body.intent.slots.Artist.value;
 
-    } else {
-        info.outputSpeech = "I didn't understand that. Please try again.";
-        res.send(JSON.stringify(info));
+        } else if (body.intent.name == "new_genre") {
+            var genre = body.intent.slots.Genre.value;
+
+        } else {
+            info.outputSpeech = "I didn't understand that. Please try again.";
+            res.send(JSON.stringify(info));
+        }
     }
+    
 
 });
 
