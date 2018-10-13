ff9b1eae1624e24b67c0f5ea433f2662d8d1bede
Aaron Epstein <a.epstein7ae@gmail.com>
2018-10-12 21:40:11 -0400

    Db models

--- api/models/session.js
+++ api/models/session.js
@@ -3,7 +3,18 @@ const mongoose = require("mongoose")
 const session = mongoose.Schema({
 	userId: string,
 	sessionId: string,
-	//current parameters
+	danceability: {min: number, max: number},
+	energy: {min: number, max: number},
+	key: {min: number, max: number},
+	loudness: {min: number, max: number},
+	mode: {min: number, max: number},
+	speechiness: {min: number, max: number},
+	acousticness: {min: number, max: number},
+	instrumentalness: {min: number, max: number},
+	liveness: {min: number, max: number},
+	valence: {min: number, max: number},
+	tempo: {min: number, max: number},
+	time_signature: {min: number, max: number}
 });
 
 module.exports = mongoose.model("Session", session);
--- api/models/songs.js
+++ api/models/songs.js
@@ -3,6 +3,22 @@ const mongoose = require("mongoose")
 const songs = mongoose.Schema({
 	sessionId: string,
 	userId: string,
+	songs: [{
+		songId: string,
+		danceability: number,
+		energy: number,
+		key: number,
+		loudness: number,
+		mode: number,
+		speechiness: number,
+		acousticness: number,
+		instrumentalness: number,
+		liveness: number,
+		valence: number,
+		tempo: number,
+		time_signature: number,
+		like: boolean
+	}]
 	//list of songs
 });
 
