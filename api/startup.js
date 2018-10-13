8bf95f4f43d75056c03d7e56fde15e7a91188181
Josh Simani <jsimani@gmail.com>
2018-10-13 01:50:03 -0400

    Finished initialize and start POSTS

--- api/startup.js
+++ api/startup.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 const mongoose = require('mongoose');
+const session = require('session');
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -17,21 +18,48 @@ app.post("/initialize", function(req, res) {
 });
 
 app.post("/start", function(req, res) {
-    var information = new Object();
-    //res.send(req.body);
+    var info = new Object();
+
     const body = req.body;
     if (body.intent.name == "start_new") {
-        information.outputSpeech = "Where should we get started?";
-        res.send(JSON.stringify(information));
+        info.outputSpeech = "Where should we get started?";
+        res.send(JSON.stringify(info));
+    } else if (body.intent.name == "resume_existing") {
+        info.outputSpeech =  "Which session should we resume?";
+        res.send(JSON.stringify(info));
+    } else {
+        info.outputSpeech = "I didn't understand that. Please try again.";
+        res.send(JSON.stringify(info));
     }
-    else if (body.intent.name == "resume_existing") {
-        information.outputSpeech =  "Which session should we resume?";
-        res.send(JSON.stringify(information));
+});
+
+app.post("/start_new", function(req, res) {
+    var info = new Object();
+    const body = req.body;
+
+    var new_sessionID = body.session.sessionID;
+    var new_session = new session({sessionID: new_sessionID});
+    
+    if (body.intent.name == "new_song") {
+        var artist = body.intent.slots.Artist.value;
+        $.get("https://api.spotify.com/v1/search?q=" + artist + "&type=artist&limit=1", function(data){
+            var song = body.intent.slots.Song.value;
+            $
+            $.get("https://api.spotify.com/v1/recommendations?seed_artists")
+        })
+        
+
+    } else if (body.intent.name == "new_artist") {
+        var artist = body.intent.slots.Artist.value;
+
+    } else if (body.intent.name == "new_genre") {
+        var genre = body.intent.slots.Genre.value;
+
+    } else {
+        info.outputSpeech = "I didn't understand that. Please try again.";
+        res.send(JSON.stringify(info));
     }
-    else {
-        information.outputSpeech = "I didn't understand that. Please try again.";
-        res.send(JSON.stringify(information));
-    }
+
 });
 
 
